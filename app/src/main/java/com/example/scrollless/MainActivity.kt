4e8d64a87d4604d506cd317730fa062883eefbac
JDSweet <djackson10151@gmail.com>
2024-10-16 19:52:35 -0500

    pushin' images to the git

--- app/src/main/java/com/example/scrollless/MainActivity.kt
+++ app/src/main/java/com/example/scrollless/MainActivity.kt
@@ -6,11 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import com.example.scrollless.ui.theme.ScrollLessTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +36,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    TextButton({ButtonClick()})
+    {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+}
+
+fun ButtonClick()
+{
+
 }
 
 @Preview(showBackground = true)
