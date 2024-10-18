e55157a81149246421e085822132ac816e073e09
JDSweet <djackson10151@gmail.com>
2024-10-18 13:46:24 -0500

    Tried to learn Jetpack Compose. We might have a UI, or we might have a problem. Who knows?

--- app/src/main/java/com/example/scrollless/MainActivity.kt
+++ app/src/main/java/com/example/scrollless/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
@@ -24,14 +26,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             ScrollLessTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    /*Greeting(
                         name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )*/
+                    GUIBackground(
+                        name = "Background",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
     }
+}
+
+@Composable
+fun GUIBackground(name: String, modifier : Modifier = Modifier)
+{
+    Image(
+        painter = painterResource(R.drawable.background_01),
+        contentDescription = "Background Image",
+        modifier = modifier
+    )
 }
 
 @Composable
