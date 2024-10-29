ced3717c77485d226bb3a8ed1936215394729bdf
JDSweet <djackson10151@gmail.com>
2024-10-29 13:09:56 -0500

    Added some white text overlaid on the background image. We need columns and other similar positioning composables to ensure text and other widgets do not overlap. We are using boxes to act as containers for some common attributes that child components share.

--- app/src/main/java/com/example/scrollless/MainActivity.kt
+++ app/src/main/java/com/example/scrollless/MainActivity.kt
@@ -7,9 +7,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -18,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.scrollless.ui.theme.ScrollLessTheme
@@ -28,20 +35,59 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ScrollLessTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    /*Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )*/
-                    GUIBackground(
-                        name = "Background",
-                        modifier = Modifier.padding(innerPadding)
+                //ScaffoldTextFields()
+                CenterText("Name 1", "Name 2", "Name 3")
+            }
+        }
+    }
+
+    @Composable
+    fun ScaffoldTextFields()
+    {
+        Box (modifier = Modifier.offset(30.dp, 30.dp)){
+            GreetingMessage(name = "Android", modifier = Modifier.padding(1.dp))
+            GreetingMessage(name = "Dustin", modifier = Modifier.padding(1.dp))
+        }
+    }
+}
+
+
+
+
+
+@Composable
+fun CenterText(vararg names: String)
+{
+    Box (modifier = Modifier.offset(0.dp, 0.dp),)
+    {
+        val imageModifier = Modifier.size(1000.dp)
+            .border(BorderStroke(1.dp, Color.Black))
+        Image (
+            painter = painterResource(R.drawable.background_01),
+            contentDescription = "Background Image",
+            contentScale = ContentScale.Fit,
+            modifier = imageModifier
+        )
+        Box(modifier = Modifier.offset(30.dp, 30.dp))
+        {
+            Column()
+            {
+                var i = 0
+                for(name in names)
+                {
+                    i++
+                    Text(
+                        text = "Hello World $i",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(20.dp),
+                        color = Color.White
                     )
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun GUIBackground(name: String, modifier : Modifier = Modifier)
@@ -53,17 +99,24 @@ fun GUIBackground(name: String, modifier : Modifier = Modifier)
         painter = painterResource(R.drawable.background_01),
         contentDescription = "Background Image",
         contentScale = ContentScale.Fit,
-        modifier = imageModifier
-    )
+        modifier = imageModifier)
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun GUIBackgroundText(name: String, modifier : Modifier = Modifier)
+{
+    Text(name, textAlign = TextAlign.Center, modifier = Modifier);
+}
+
+@Composable
+fun GreetingMessage(name: String, modifier: Modifier = Modifier) {
     TextButton({buttonClick()})
     {
         Text(
             text = "Hello $name!",
-            modifier = modifier
+            modifier = modifier,
+            color = Color.White
         )
     }
 }
@@ -76,6 +129,6 @@ fun buttonClick(): Boolean {
 @Composable
 fun GreetingPreview() {
     ScrollLessTheme {
-        Greeting("Android")
+        GreetingMessage("Android")
     }
 }
